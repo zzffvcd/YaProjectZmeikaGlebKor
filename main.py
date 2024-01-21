120e9d079aeae4d2c2dc670f5d0664775a2ce73b
zzffvcd <135701492+zzffvcd@users.noreply.github.com>
2024-01-21 17:29:40 +0300

    Update main.py
    - Вторая версия змейки

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import pygame
 import sys
+import random
 
 pygame.init()
-size = WIDTH, HEIGHT = 800, 400
+size = WIDTH, HEIGHT = 1200, 600
 screen = pygame.display.set_mode(size)
 FPS = 50
 clock = pygame.time.Clock()
@@ -29,7 +30,11 @@ def load_image(name, color_key=None):
 tile_images = {
     'empty': load_image('grass.jpg')
 }
-player_image = load_image('G_zmeya.png')
+player_image = load_image('golova.png', -1)
+tail = load_image('zmeya.png', -1)
+tail_tail = load_image('tail.png', -1)
+povor = load_image('povorotzmeu.png', -1)
+apple = load_image('apple.png', -1)
 
 tile_width = tile_height = 50
 
@@ -57,12 +62,36 @@ class Tile(pygame.sprite.Sprite):
             tile_width * pos_x, tile_height * pos_y)
 
 
+class Apple(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(tt_group, all_sprites)
+        self.image = apple
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y + 2)
+
+
+class Tail_tail(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(tt_group, all_sprites)
+        self.image = tail_tail
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Tail(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(tt_group, all_sprites)
+        self.image = tail
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
 class Zmeya(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(zm_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
+            tile_width * pos_x, tile_height * pos_y + 2)
 
 
 def generate_level(level):
@@ -74,63 +103,67 @@ def generate_level(level):
             elif level[y][x] == '@':
                 Tile('empty', x, y)
                 new_zm = Zmeya(x, y)
-    return new_zm, x, y
+    x1 = random.randint(1, 23)
+    y1 = random.randint(1, 11)
+    applee = Apple(x1, y1)
+    tail = Tail(0, 0)
+    Taill = Tail_tail(0, 0)
+    return new_zm, x, y, applee, x1, y1, tail, Taill
 
 
 def start_screen():
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    fon = pygame.transform.scale(load_image('fon.png'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
-    timer = 400
     while True:
-        timer -= 1
-        if timer == 0:
-            return
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 return
         pygame.display.flip()
         clock.tick(FPS)
-        # TODO сделать меню с кнопками
-
-
-class Camera:
-    def __init__(self):
-        self.dx = 0
-        self.dy = 0
-
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
 
 
 start_screen()
-zmi, level_x, level_y = generate_level(load_level('pole.txt'))
+picture = pygame.image.load('Data/map.png')
+rect = picture.get_rect()
+rect = rect.move((0, 0))
+zmi, level_x, level_y, applee, xap, yap, tail, Tail_tail = generate_level(load_level('pole.txt'))
 running = True
-camera = Camera()
-napr = '-y'
-# TODO змейка чёрная хотя фон должен быть пустым
-coor = [5, 5]  # координаты змеи
+napr = '-x'
+coor = [5, 5]
 timer = 75
+posl = '+x'
+rotate = 0
+Tail_tail.rect.x = SHAG * (coor[0] + 1)
+Tail_tail.rect.y = SHAG * (coor[1] - 1)
+tail.rect.x = SHAG * (coor[0]) - 20
+tail.rect.y = SHAG * (coor[1] - 1)
+applee.rect.y, applee.rect.x = SHAG * yap, SHAG * xap
+apcoor = [xap + 1, yap + 1]
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and posl != '+y':
+                zmi.image = pygame.transform.rotate(player_image, 270)
                 napr = '-y'
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT and posl != '+x':
+                zmi.image = pygame.transform.rotate(player_image, 0)
                 napr = '-x'
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN and posl != '-y':
+                zmi.image = pygame.transform.rotate(player_image, 90)
                 napr = '+y'
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT and posl != '-x':
+                zmi.image = pygame.transform.rotate(player_image, 180)
                 napr = '+x'
+    if apcoor[0] == coor[0] and apcoor[1] == coor[1]:
+        xx = random.randint(1, 23)
+        yy = random.randint(1, 11)
+        applee.rect.y, applee.rect.x = SHAG * yy, SHAG * xx
+        apcoor = [xx + 1, yy + 1]
+    print(coor, apcoor, applee.rect.x)
     timer -= 1
     if timer == 0:
         timer = 75
@@ -143,8 +176,8 @@ while running:
         elif napr == '-x':
             coor[0] -= 1
             if coor[0] == 0:
-                zmi.rect.x += SHAG * 11
-                coor[0] = 11
+                zmi.rect.x += SHAG * 24
+                coor[0] = 24
             zmi.rect.x -= SHAG
         elif napr == '+y':
             coor[1] += 1
@@ -154,16 +187,15 @@ while running:
             zmi.rect.y += SHAG
         elif napr == '+x':
             coor[0] += 1
-            if coor[0] == 12:
-                zmi.rect.x -= SHAG * 11
+            if coor[0] == 25:
+                zmi.rect.x -= SHAG * 24
                 coor[0] = 1
             zmi.rect.x += SHAG
-    camera.update(zmi)
-    for sprite in all_sprites:
-        camera.apply(sprite)
-    screen.fill(pygame.Color("green"))  # TODO сделать нормальный фон а дальше музыку.
+        posleposl = posl
+        posl = napr
+
+    screen.blit(pygame.transform.scale(load_image("map.png"), (WIDTH, HEIGHT)), (0, 0))  # TODO сделать музыку.
     tt_group.draw(screen)
     zm_group.draw(screen)
     pygame.display.flip()
     clock.tick(100)
-
